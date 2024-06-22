52c6e27f29d2741b9c17b82f4f496538e06a2103
KushnirDanil <dankusnir09@gmail.com>
2024-06-22 14:29:14 +0300

    1

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    n = random.randint(0, 1000)
-    k = random.randint(1000, 5000)
-    return render_template('index.html', n=n, k=k)
+    return render_template('index.html')
 
 
 @app.route('/login')
@@ -16,5 +13,7 @@ def login():
     return render_template('login.html')
 
 
+# Лише для локального сервера  (за коментувати)
 if __name__ == '__main__':
     app.run(debug=True)
+
