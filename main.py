4657cc4a43bed3ff88017b70a24caed1c148859d
KushnirDanil <dankusnir09@gmail.com>
2024-06-29 14:09:56 +0300

    1

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def login():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 @app.route('/articles')
 def articles():
