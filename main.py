ed5eac1c6dd0005272c89a3ba0a94b8c41c5b477
KushnirDanil <dankusnir09@gmail.com>
2024-06-29 13:14:46 +0300

    1

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
+
+@app.route('/')
+@app.route('/name')
+def index(name=''):
+    return render_template('index.html', name=name)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
 
 # Лише для локального сервера  (за коментувати)
 if __name__ == '__main__':
